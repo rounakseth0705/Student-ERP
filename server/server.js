ddf117a8bc08c106d7ae3c302af89572dbb1f4fe
Rounak Seth <sethrounak7@gmail.com>
2026-05-25 03:04:39 +0530

    Fix Cors

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ import attendenceRouter from "./routes/attendenceRoutes.js";
 import assignmentUploadRouter from "./routes/assignmentUploadRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 connectDB();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://student-erp-frontend.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/course", courseRouter);
