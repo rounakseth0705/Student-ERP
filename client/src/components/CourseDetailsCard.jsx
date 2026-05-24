5a30ecc8a0a1b6b30b782f620cafc97a5e126cfb
Rounak Seth <sethrounak7@gmail.com>
2026-05-25 03:18:37 +0530

    Fix course details card

--- client/src/components/CourseDetailsCard.jsx
+++ client/src/components/CourseDetailsCard.jsx
@@ -1,10 +1,10 @@
 const CourseDetailsCard = ({ courseName, courseCode, duration, semesters }) => {
     return(
-        <div className="flex flex-col justify-center bg-blue-400 text-white shadow-xl rounded px-8 py-3 sm:py-8 sm:text-3xl md:text-4xl md:p-10">
-            <h1 className="m-2 p-1 sm:p-2">Course name : {courseName}</h1>
-            <h1 className="m-2 p-1 sm:p-2">Course code : {courseCode}</h1>
-            <h1 className="m-2 p-1 sm:p-2">Course Duration : {duration} years</h1>
-            <h1 className="m-2 p-1 sm:p-2">Total Semesters : {semesters}</h1>
+        <div className="flex flex-col justify-center text-gray-700 px-8 py-3 text-sm sm:py-5 sm:text-2xl lg:py-6 lg:px-10">
+            <h1 className="bg-white rounded-2xl shadow-xl m-2 px-12 py-3 sm:px-12 sm:py-4">Course name :- {courseName}</h1>
+            <h1 className="bg-white rounded-2xl shadow-xl m-2 px-12 py-3 sm:px-12 sm:py-4">Course code :- {courseCode}</h1>
+            <h1 className="bg-white rounded-2xl shadow-xl m-2 px-12 py-3 sm:px-12 sm:py-4">Course Duration :- {duration} years</h1>
+            <h1 className="bg-white rounded-2xl shadow-xl m-2 px-12 py-3 sm:px-12 sm:py-4">Total Semesters :- {semesters}</h1>
         </div>
     )
 }
